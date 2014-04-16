2050833284254b45eefcf59e4c1d71708624fe11
Shokat Saifi <shokatsaifi@gmail.com>
2014-04-16 17:42:54 +0530

    initial commit

--- http.php
+++ http.php
@@ -1,8 +1,7 @@
 <?php
 function redirect($url) {
 if (!headers_sent()) {
-header('Location: http://' . $_SERVER['HTTP_HOST'] .
-dirname($_SERVER['PHP_SELF']) . '/' . $url);
+header('Location: http://' . $_SERVER['HTTP_HOST']) ;
 } else {
 die('Could not redirect; Headers already sent (output).');
 }
