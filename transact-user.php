e2c74a926d2103cf0bdb5cbc463a37b334c4ff2e
Shokat Saifi <shokatsaifi@gmail.com>
2014-04-20 14:13:33 +0530

    fixed bug

--- transact-user.php
+++ transact-user.php
@@ -1,8 +1,6 @@
 <?php
 require_once 'conn.php';
-require_once 'http.php';
-if (isset($_REQUEST['action'])) {
-<<<<<<< HEAD
+require_once 'http.php';if (isset($_REQUEST['action'])) {
 switch ($_REQUEST['action']) {
 case 'Login':
 if (isset($_POST['email'])
@@ -20,8 +18,6 @@ session_start();
 $_SESSION['user_id'] = $row['user_id'];
 $_SESSION['access_lvl'] = $row['access_lvl'];
 $_SESSION['name'] = $row['name'];
-} else {
-    redirect(admin.php);
 }
 }
 redirect('index.php');
@@ -68,71 +64,6 @@ or die('Could not update user account; ' . mysql_error());
 redirect('admin.php');
 break;
 case 'Send my reminder!':
-=======
-    switch ($_REQUEST['action']) {
-        case 'Login':
-            if (isset($_POST['email'])
-                    and isset($_POST['passwd']))
-                {
-                $sql = "SELECT user_id, access_lvl,name " .
-                        "FROM cms_users " .
-                        "WHERE email='" . $_POST['email'] . "' " .
-                        "AND passwd='" . $_POST['passwd'] . "'";
-                $result = mysql_query($sql, $conn)
-                        or die('Could not look up user information; ' .
-                        mysql_error());
-                if ($row = mysql_fetch_array($result)) {
-                    session_start();
-                    $_SESSION['user_id'] = $row['user_id'];
-                    $_SESSION['access_lvl'] = $row['access_lvl'];
-                    $_SESSION['name'] = $row['name'];
-                    
-                    }
-                 }
-                 redirect('index.php');
-        case 'Logout':
-            session_start();
-            session_unset();
-            session_destroy();
-            redirect('index.php');
-            break;
-        case 'Create Account':
-            if (isset($_POST['name'])
-                    and isset($_POST['email'])
-                    and isset($_POST['passwd'])
-                    and isset($_POST['passwd2'])
-                    and $_POST['passwd'] == $_POST['passwd2'])
-                {
-                $sql = "INSERT INTO cms_users (email, name, passwd) " .
-                        "VALUES ('" . $_POST['email'] . "','" .
-                        $_POST['name'] . "','" . $_POST['passwd'] . "')";
-                mysql_query($sql, $conn)
-                        or die('Could not create user account; ' . mysql_error());
-                session_start();
-                $_SESSION['user_id'] = mysql_insert_id($conn);
-                $_SESSION['access_lvl'] = 1;
-                $_SESSION['name'] = $_POST['name'];
-                
-                }
-                redirect('index.php');
-                break;
-        case 'Modify Account':
-            if (isset($_POST['name'])
-                    and isset($_POST['email'])
-                    and isset($_POST['accesslvl'])
-                    and isset($_POST['userid'])){
-                $sql = "UPDATE cms_users " .
-                        "SET email='" . $_POST['email'] .
-                        "', name='" . $_POST['name'] .
-                        "', access_lvl=" . $_POST['accesslvl'] . " " .
-                        " WHERE user_id=" . $_POST['userid'];
-                mysql_query($sql, $conn)
-                        or die('Could not update user account; ' . mysql_error());
-                }
-                redirect('admin.php');
-                break;
-        case 'Send my reminder!':
->>>>>>> 18a31d2c59cb86f36387c975825f6aa1225d8ad6
 if (isset($_POST['email'])) {
 $sql = "SELECT passwd FROM cms_users " .
 "WHERE email='" . $_POST['email'] . "'";
@@ -153,21 +84,21 @@ or die('Could not send reminder email.');
 }
 redirect('login.php');
 break;
-         case 'Change my info':
-             session_start();
-             if (isset($_POST['name'])
-                     and isset($_POST['email'])
-                     and isset($_SESSION['user_id'])){
-                 $sql = "UPDATE cms_users " .
-                         "SET email='" . $_POST['email'] .
-                         "', name='" . $_POST['name'] . "' " .
-                         "WHERE user_id=" . $_SESSION['user_id'];
-                 mysql_query($sql, $conn)
-                         or die('Could not update user account; ' . mysql_error());
-                 
-                         }
-                         redirect('cpanel.php');
-                         break;
-                         }
+case 'Change my info':
+session_start();
+if (isset($_POST['name'])
+and isset($_POST['email'])
+and isset($_SESSION['user_id']))
+{
+$sql = "UPDATE cms_users " .
+"SET email='" . $_POST['email'] .
+        "', name='" . $_POST['name'] . "' " .
+"WHERE user_id=" . $_SESSION['user_id'];
+mysql_query($sql, $conn)
+or die('Could not update user account; ' . mysql_error());
+}
+redirect('cpanel.php');
+break;
+}
 }
 ?>
